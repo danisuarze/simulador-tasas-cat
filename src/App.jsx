1ab59b9aa170423d7778bc8e13b6b874a545514a
Daniel <danisuarze@gmail.com>
2026-07-15 03:39:35 -0300

    agrego menu de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,51 @@
-import FooterC from "./components/footer/FooterC"
-import JumbotronC from "./components/jumbotron/JumbotronC"
-import PresentacionSimuladoresC from "./components/presentacionSimuladores/PresentacionSimuladoresC"
+import { useState } from "react";
+import FooterC from "./components/footer/FooterC";
+import JumbotronC from "./components/jumbotron/JumbotronC";
+import MenuArrastrableC from "./components/menuInicio/MenuArrastrableC";
+import CardsC from "./components/cards/CardsC";
+import HonorariosC from "./components/honorarios/HonorariosC";
 
+const App = () => {
+  const [activeComponent, setActiveComponent] = useState(null);
 
-const App = () => { 
+  const handleSelectOption = (option) => {
+    setActiveComponent(option);
+  };
 
-  
+  const handleBack = () => {
+    setActiveComponent(null);
+  };
+
+  const renderContent = () => {
+    switch (activeComponent) {
+      case "tasa":
+        return <CardsC onBack={handleBack} />;
+      case "honorarios":
+        return <HonorariosC onBack={handleBack} />;
+      default:
+        return null;
+    }
+  };
+
+  const showMenu = activeComponent === null;
+
   return (
-    <>
-    <JumbotronC />
-    <PresentacionSimuladoresC />
-    <FooterC />
-    </>
-  )
+    <div style={{ 
+      display: 'flex', 
+      flexDirection: 'column', 
+      minHeight: '100vh',
+      background: '#111B4D'
+    }}>
+      <JumbotronC />
+      {showMenu && <MenuArrastrableC onSelectOption={handleSelectOption} />}
+      {!showMenu && (
+        <div style={{ flex: 1, padding: '20px' }}>
+          {renderContent()}
+        </div>
+      )}
+      <FooterC />
+    </div>
+  );
+};
 
-}
-
-export default App 
+export default App;
