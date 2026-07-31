304b0b7147b13fcc023356499b80c22ae1766b3d
Daniel Suarez <Daniel Suarez>
2026-07-31 14:26:32 -0300

    Corrijo TasacionesC

---
+++ .vite/deps/react-bootstrap_Container.js
@@ -0,0 +1,10 @@
+"use client";
+import {
+  Container_default
+} from "./chunk-3ELWVSU6.js";
+import "./chunk-P7OK6MOX.js";
+import "./chunk-3VDXVL42.js";
+import "./chunk-5WRI5ZAA.js";
+export {
+  Container_default as default
+};
---
+++ .vite/deps/react-bootstrap_Form.js
@@ -0,0 +1,10 @@
+import {
+  Form_default
+} from "./chunk-ADYJBKAC.js";
+import "./chunk-LL3LRR3V.js";
+import "./chunk-P7OK6MOX.js";
+import "./chunk-3VDXVL42.js";
+import "./chunk-5WRI5ZAA.js";
+export {
+  Form_default as default
+};
